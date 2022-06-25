d12623cbb8060a24ccd5b23360f1f9e093e7907c
DOAGuruinfosystems <DOAGuruinfosystems@gmail.com>
2022-06-25 18:43:13 +0530

    first commit

--- nmp/src/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-/*****create connection to DB */
-mongoose.connect("mongodb://localhost:27017/mynewdb")
-.then(()=>{
-    console.log("connection Successful");
-}).catch((err)=>{
-    console.log("Connection Error")
-})
